7fd0f37703e70823e4779c50323eb1af7872b2ed
Alba Sierra <albasd2001@gmail.com>
2024-10-21 13:12:41 +0200

    Contacto Service

--- src/main/java/com/example/agenda/service/ContactoService.java
+++ src/main/java/com/example/agenda/service/ContactoService.java
@@ -1,4 +1,47 @@
 package com.example.agenda.service;
+import com.example.agenda.model.Contacto;
+import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+@Service
 public class ContactoService {
+    private Map<String, Contacto> agenda = new HashMap<>();
+
+    public Map<String, Contacto> obtenerContactos(){
+        return agenda;
+    }
+
+    //Eliminar un contacto
+    public boolean eliminarContacto(String nombre){
+        Contacto contacto = agenda.get(nombre);
+
+        if(contacto!= null && !contacto.isFavorito()){
+            agenda.remove(nombre);
+            return true;
+        }
+
+        return false;//Si es favorito no se puede eliminar
+    }
+
+    //Buscar un contacto por nombre
+    public Contacto obtenerContactoPorNombre(String nombre){
+        return agenda.get(nombre);
+    }
+
+    //Actualiza un contacto
+    public void actualizarContacto(String nombreOriginal, Contacto contactoActualizado){
+        if(!nombreOriginal.equals(contactoActualizado.getNombre())){
+            agenda.remove(nombreOriginal); //eliminar el contacto original
+        }
+        agenda.put(contactoActualizado.getNombre(),contactoActualizado);
+    }
+
+    //cambiar el estado favorito de un contacto
+    public void cambiarFavorito(String nombre){
+        Contacto contacto = agenda.get(nombre);
+        if(contacto!=null){
+            contacto.setFavorito(!contacto.isFavorito());
+        }
+    }
 }
